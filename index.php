b967e1fbae5b832d3da163b9973df1f3d7de0528
Rakesh-sudo-stack <rakeshkumar1957830@gmail.com>
2022-11-30 19:29:50 +0530

    1st commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script defer src="https://kit.fontawesome.com/40bc81af5c.js" crossorigin="anonymous"></script>
+    <style>
+        <?php
+        include './css/style.css';
+        ?>
+    </style>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="">home</a></li>
+            <li class="dropdown-nav-li">
+                <a href="">about us<i class="fa fa-angle-down"></i></a>
+                <ul>
+                    <li><a href="">Aims & Objectives</a></li>
+                    <li><a href="">Our Constitution</a></li>
+                    <li><a href="">Director Message</a></li>
+                    <li><a href="">School Managing Committee</a></li>
+                </ul>
+            </li>
+            <li class="dropdown-nav-li">
+                <a href="">school info<i class="fa fa-angle-down"></i></a>
+                <ul>
+                    <li><a href="">school profile</a></li>
+                    <li><a href="">affidavit/declaration</a></li>
+                    <li><a href="">school advisory board</a></li>
+                </ul>
+            </li>
+            <li class="dropdown-nav-li">
+                <a href="">academics<i class="fa fa-angle-down"></i></a>
+                <ul>
+                    <li><a href="">academic calender</a></li>
+                    <li><a href="">teachers list</a></li>
+                    <li><a href="">book list</a></li>
+                    <li><a href="">academic achievements</a></li>
+                    <li><a href="">code of conduct for students</a></li>
+                    <li><a href="">advice to parents</a></li>
+                    <li><a href="">activities&competitions</a></li>
+                    <li><a href="">annual sports meet</a></li>
+                    <li><a href="">exam schedule</a></li>
+                    <li><a href="">school uniform</a></li>
+                    <li><a href="">holiday list</a></li>
+                </ul>
+            </li>
+            <li class="dropdown-nav-li"><a href="">
+                admission<i class="fa fa-angle-down"></i></a>
+                <ul>
+                    <li><a href="">admission enquiry</a></li>
+                    <li><a href="">admission guideline</a></li>
+                </ul>
+            </li>
+            <li class="dropdown-nav-li">
+                <a href="">gallery<i class="fa fa-angle-down"></i></a>
+                <ul>
+                    <li><a href="">photo gallery</a></li>
+                    <li><a href="">video gallery</a></li>
+                </ul>
+            </li>
+            <li><a href="">downloads</a></li>
+            <li><a href="">transfer certificate</a></li>
+            <li><a href="">alumini</a></li>
+            <li><a href="">contact us</a></li>
+            <li class="highlight"><a href="">pay fees online</a></li>
+        </ul>
+    </nav>
+    <header>
+        <a href="index.php"><img src="./img/bdps_logo.png" alt=""></a>
+        <ul>
+            <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>Registration for Class XI 2022-2023</a></li>
+            <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>TALENT SEARCH EXAMINATION II</a></li>
+            <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>Vacancy-2022</a></li>
+            <li><a href="" class="col-change"><i class="fa fa-user"></i>Registration for Nursery to Class IX</a></li>
+            <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>Topper's List</a></li>
+            <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>Sawan Mohatsava</a></li>
+        </ul>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
