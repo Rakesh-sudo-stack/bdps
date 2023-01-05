7cabe93b4280ec707f2b7ded64a126e42feaa0cc
Rakesh-sudo-stack <76909034+Rakesh-sudo-stack@users.noreply.github.com>
2023-01-05 17:10:53 +0530

    New commit

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <script defer src="https://kit.fontawesome.com/40bc81af5c.js" crossorigin="anonymous"></script>
     <style>
         <?php
+        include './css/global.css';
         include './css/style.css';
         ?>
     </style>
@@ -75,16 +76,82 @@
         <div class="menu-btn"><i id="menu-ico" class="fa-solid fa-bars"></i></div>
         <ul>
             <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>Registration for Class XI 2022-2023</a></li>
-            <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>TALENT SEARCH EXAMINATION II</a></li>
             <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>Vacancy-2022</a></li>
             <li><a href="" class="col-change"><i class="fa fa-user"></i>Registration for Nursery to Class IX</a></li>
             <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>Topper's List</a></li>
             <li><a href="" class="col-change"><i class="fa fa-graduation-cap"></i>Sawan Mohatsava</a></li>
         </ul>
     </header>
-    <main></main>
+    <marquee class="movable-notice" behavior="scroll" direction="">
+        <ul>
+            <li class="col-change">Registration for Nur - IX for 2023-2024 will start from 15/12/2022</li>
+        </ul>
+    </marquee>
+    <main>
+    <div class="container">
+
+        <div class="mySlides">
+            <div class="numbertext">1 / 10</div>
+            <img src="./img/heroes-section/img5.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">2 / 6</div>
+            <img src="./img/heroes-section/img2.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">3 / 6</div>
+            <img src="./img/heroes-section/img3.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">4 / 6</div>
+            <img src="./img/heroes-section/img4.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">5 / 6</div>
+            <img src="./img/heroes-section/img5.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">6 / 6</div>
+            <img src="./img/heroes-section/img6.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">5 / 6</div>
+            <img src="./img/heroes-section/img7.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">5 / 6</div>
+            <img src="./img/heroes-section/img8.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">5 / 6</div>
+            <img src="./img/heroes-section/img9.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides">
+        <div class="numbertext">5 / 6</div>
+            <img src="./img/heroes-section/img10.jpg" style="width:100%">
+        </div>
+
+        <!-- Next and previous buttons -->
+        <a class="prev-image">&#10094;</a>
+        <a class="next-image">&#10095;</a>
+
+        <!-- Image text -->
+        <div class="caption-container">
+        <p id="caption"></p>
+        </div>
+</div>
+    </main>
     <footer></footer>
-    <script>
+    <script defer>
         <?php
         include './js/jquery-min.js';
         include './js/script.js';
