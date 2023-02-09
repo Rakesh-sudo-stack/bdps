ae517529e4ddee9128c7b0cdd87e2dc2ebad47da
Rakesh-sudo-stack <76909034+Rakesh-sudo-stack@users.noreply.github.com>
2023-02-09 19:00:16 +0530

    new commit

--- pages/aims.php
+++ pages/aims.php
@@ -6,15 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AIMS & OBJECTIVES</title>
     <link rel="stylesheet" href="../static/css/global.css">
-    <link rel="stylesheet" href="../static/css/style.css">
+    <link rel="stylesheet" href="../static/css/aims.css">
 </head>
 <body>
     <?php
         include '../header.html';
     ?>
-    <main></main>
+    <main>
+        <div class="page-hero">
+            <img src="../static/img/heroes-section/img10.jpg" alt="">
+            <h1>Aims & Objectives</h1>
+        </div>
+    </main>
     <?php
         include '../footer.html';
     ?>
+    <script src="../static/js/jquery-min.js"></script>
+    <script src="../static/js/script.js"></script>
 </body>
 </html>
