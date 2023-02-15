e8ca8d4f001b1117845ef5d8be371e12f15ecf5a
Rakesh-sudo-stack <rakeshkumar1957830@gmail.com>
2023-02-15 19:12:21 +0530

    folder structure upgraded

--- pages/aims.php
+++ pages/aims.php
@@ -5,23 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AIMS & OBJECTIVES</title>
+    <script defer src="https://kit.fontawesome.com/40bc81af5c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../static/css/global.css">
     <link rel="stylesheet" href="../static/css/aims.css">
+    <link rel="icon" type="image/x-icon" href="./static/img/bdps_logo_4.webp">
 </head>
 <body>
     <?php
-        include '../header.html';
+        include './templates/header.html';
     ?>
     <main>
         <div class="page-hero">
-            <img src="../static/img/heroes-section/img10.jpg" alt="">
+            <img src="../static/img/heroes-section/img10.webp" alt="">
             <h1>Aims & Objectives</h1>
         </div>
     </main>
     <?php
-        include '../footer.html';
+        include './templates/footer.html';
     ?>
-    <script src="../static/js/jquery-min.js"></script>
     <script src="../static/js/script.js"></script>
 </body>
 </html>
