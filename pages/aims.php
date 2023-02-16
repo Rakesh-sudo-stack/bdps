604b9fb8c27fb3479eb452ff8061d8f43ac29c11
Rakesh-sudo-stack <76909034+Rakesh-sudo-stack@users.noreply.github.com>
2023-02-16 18:37:02 +0530

    commit

--- pages/aims.php
+++ pages/aims.php
@@ -5,10 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AIMS & OBJECTIVES</title>
-    <script defer src="https://kit.fontawesome.com/40bc81af5c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../static/css/global.css">
     <link rel="stylesheet" href="../static/css/aims.css">
-    <link rel="icon" type="image/x-icon" href="./static/img/bdps_logo_4.webp">
+    <script defer src="https://kit.fontawesome.com/40bc81af5c.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <?php
@@ -19,10 +18,17 @@
             <img src="../static/img/heroes-section/img10.webp" alt="">
             <h1>Aims & Objectives</h1>
         </div>
+        <div class="bread-crumb">
+            <ol>
+                <li class='active'><i class='fa fa-home'></i><a href='../index.php'>Home</a></li>
+                <li>Aims & Objectives</li>
+            </ol>
+        </div>
     </main>
     <?php
         include './templates/footer.html';
     ?>
+    <script src="../static/js/jquery-min.js"></script>
     <script src="../static/js/script.js"></script>
 </body>
 </html>
