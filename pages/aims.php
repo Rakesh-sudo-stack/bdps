40ca0356b61e3cfe21d7910f0aae08758743a05f
Rakesh-sudo-stack <76909034+Rakesh-sudo-stack@users.noreply.github.com>
2023-02-19 18:55:34 +0530

    new commit

--- pages/aims.php
+++ pages/aims.php
@@ -6,29 +6,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AIMS & OBJECTIVES</title>
     <link rel="stylesheet" href="../static/css/global.css">
+    <link rel="stylesheet" href="../static/css/page.css">
     <link rel="stylesheet" href="../static/css/aims.css">
+    <link rel="icon" type="image/x-icon" href="../static/img/bdps_logo_4.webp">
     <script defer src="https://kit.fontawesome.com/40bc81af5c.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <?php
         include './templates/header.html';
     ?>
+    <div class="page-hero">
+        <img src="../static/img/heroes-section/img10.webp" alt="OUR TEAM">
+        <h1>Aims & Objectives</h1>
+    </div>
+    <div class="bread-crumb">
+        <ol>
+            <li class='active text-bold'><i class='fa fa-home'></i><a href='../index.php'>Home</a></li>
+            <li class='active'><a href='./aims.php'>Aims & Objectives</a></li>
+        </ol>
+    </div>
     <main>
-        <div class="page-hero">
-            <img src="../static/img/heroes-section/img10.webp" alt="">
-            <h1>Aims & Objectives</h1>
+        <div class="content">
+        <p>
+            At <span class='text-bold'>B.D. PUBLIC SCHOOL</span>, we believe every child has the right to equality of opportunity and access to the whole curriculum. We aim to provide every child a learning environment:
+        </p>
+        <ul>
+            <li>is safe and secure, is exciting, stimulating, challenging and relevant.</li>
+            <li>develops the confidence and talent of each individual.</li>
+            <li>fosters esteem, self-awareness and motivate learners.</li>
+            <li>promotes the quality of resourceful,reflective and resilient,is appropriate and matched to individual needs.</li>
+            <li>offers a wide range of activities, and includes the requirement of National Curriculum.</li>
+        </ul>
         </div>
-        <div class="bread-crumb">
-            <ol>
-                <li class='active'><i class='fa fa-home'></i><a href='../index.php'>Home</a></li>
-                <li>Aims & Objectives</li>
-            </ol>
-        </div>
+        <img height=30% width=30% src="../static/img/pages/aims.webp" alt="AIMS">
     </main>
     <?php
         include './templates/footer.html';
     ?>
-    <script src="../static/js/jquery-min.js"></script>
     <script src="../static/js/script.js"></script>
 </body>
 </html>
